8a8618c741ab0b7e3936020e57b6b33909da0d81
Brent Ahrens <cidida@gmail.com>
2019-02-26 10:47:17 -0800

    adds fetch request to flask

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.getShoes()
+  }
+
+  getShoes = async () => {
+    try {
+      const shoeResponse = await fetch('http://localhost:8000/api/v1/shoes')
+      
+      const shoeParsed = await shoeResponse.json()
+
+      console.log(shoeParsed)
+    } catch(err) {
+      console.log(err)
+    }
+  }
   render() {
     return (
       <div className="App">
