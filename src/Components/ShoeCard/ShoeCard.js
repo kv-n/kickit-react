0b1752cefacea9c4a267a6d681b5fdf69d5fdcaa
Brent Ahrens <cidida@gmail.com>
2019-02-27 12:14:46 -0800

    adds styling to profile page

--- src/Components/ShoeCard/ShoeCard.js
+++ src/Components/ShoeCard/ShoeCard.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
+import './ShoeCard.css'
 
 const ShoeCard = (props) =>
-  <div className="ShoeCard-Container">
-    {console.log(props)}
-    <h1 className="shoeCard">Brand: {props.shoe.brand}</h1>
-    <h1 className="shoeCard">{props.shoe.name}</h1>
-    <div className="shoeCard">{props.shoe.picture}</div>
-    <h1 className="shoeCard">Size: {props.shoe.size}</h1>
-    <h1 cclassName="shoeCard">${props.shoe.price}</h1>
-  </div>
+    <div className="ShoeCard-Container">
+      <h1 className="shoeCard">Brand: {props.shoe.brand}</h1>
+      <h1 className="shoeCard">{props.shoe.name}</h1>
+      <div className="shoeCard">{props.shoe.picture}</div>
+      <h1 className="shoeCard">Size: {props.shoe.size}</h1>
+      <h1 className="shoeCard">${props.shoe.price}</h1>
+    </div>
 
   
 export default ShoeCard
