b5bd9d9c134f1ba241ac79e9bd964bc5083cf9d4
Brent Ahrens <cidida@gmail.com>
2019-02-27 11:45:53 -0800

    adds redirect from profile to shoe page

--- src/Components/ShoeCard/ShoeCard.js
+++ src/Components/ShoeCard/ShoeCard.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 const ShoeCard = (props) =>
   <div className="ShoeCard-Container">
     {console.log(props)}
-    <h1 className="brand">Brand: {props.shoe.brand}</h1>
-    <h1 className="name">{props.shoe.name}</h1>
-    <div className="picture">{props.shoe.picture}</div>
-    <h1 className="size">Size: {props.shoe.size}</h1>
-    <h1 className="price">${props.shoe.price}</h1>
+    <h1 className="shoeCard">Brand: {props.shoe.brand}</h1>
+    <h1 className="shoeCard">{props.shoe.name}</h1>
+    <div className="shoeCard">{props.shoe.picture}</div>
+    <h1 className="shoeCard">Size: {props.shoe.size}</h1>
+    <h1 cclassName="shoeCard">${props.shoe.price}</h1>
   </div>
 
   
