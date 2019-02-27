1081d34975e14075262c34a551e1406509397399
Brent Ahrens <cidida@gmail.com>
2019-02-27 11:18:27 -0800

    adds register and profile forms

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShoeIndex from './Components/ShoeIndex/ShoeIndex'
 import Profile from './Components/Profile/Profile'
 import { Route, Switch } from 'react-router-dom'
 import Splash from './Components/Landing/Landing'
+import Register from './Components/Register/Register'
 
 class App extends Component {
   state = {
@@ -30,6 +31,27 @@ class App extends Component {
   //     console.log(err)
   //   }
   // }
+
+  handleRegister = async (data) => {
+    try {
+      const registerResponse = await fetch('http://localhost:8000/api/v1/users', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+
+      const registerParsed = await registerResponse.json()
+      console.log(registerParsed)
+
+    } catch (err){
+      console.log(err)
+    }
+  }
+
+
   render() {
     const { shoes, users } = this.state 
     return (
@@ -38,6 +60,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path={'/'} component={() => <Splash />} />
+          <Route exact path={'/register'} component={() => <Register  handleRegister={this.handleRegister}/>} />
           <Route exact path={'/shoes'} component={() => <ShoeIndex />} />
           <Route exact path={'/shoes/:id'} component={() => <ShowShoe />} />
           <Route exact path={'/profile'} component={() => <Profile />} />
