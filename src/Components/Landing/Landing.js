89a64598e07f5bdbce339a751f2bfc419a98afa7
Kevin <k.cuison@gmail.com>
2019-02-27 16:44:25 -0800

    adds login to splash

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Landing.css'
+import Login from '../../Login/Login'
 
 const Splash = () => {
     return (
         <div className="splash-container">
         <div className="splash">
             <h1>//Kickit.</h1>
-
+            <Login/>
             <video id="bgvid" style={{ width: '100%' }}
                 src="/kickit.mp4"
                 loop
