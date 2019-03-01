6e831219a22c9448480facc2a20e87cea96d9e20
Kevin <k.cuison@gmail.com>
2019-03-01 15:47:59 -0800

    fixing logo

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -7,9 +7,19 @@ const Splash = (props) => {
     return (
         <div className="splash-container">
         <div className="splash">
-            <Link to="/shoes"><h1>//Kickit.</h1></Link>
+            {/* <Link to="/shoes"><h1>//Kickit.</h1></Link> */}
 
-            
+            <div className='container'>
+  Kick 
+  <div className='flip'>
+    <div><div>Jordan</div></div>
+    <div><div>Nike</div></div>
+    <div><div>Adidas</div></div>
+  </div>
+  It
+</div>
+
+
             <video id="bgvid" style={{ width: '100%' }}
                 src="/kickit.mp4"
                 loop
