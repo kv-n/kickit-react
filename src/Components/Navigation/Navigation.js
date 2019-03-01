99ee916aef03666cd798d13fa88e4caaf1354200
Brent Ahrens <cidida@gmail.com>
2019-03-01 13:36:29 -0800

    adds ternary to nav bar

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -13,13 +13,19 @@ const Navigation = (props) => {
         <Link to='/'><img className="logo-nav" src="https://i.imgur.com/sWeO4J0.png" alt="shoes"/></Link>
             <ul className="nav-flex">
                 <li id="list1" className="nav-list"><Link to='/shoes'>//Kicks</Link></li>
-                <li id="list2" className="nav-list"><Link to='/profile'>//Profile</Link></li>
+                <li id="list2" className="nav-list"><Link to='/profile'>//Add to Collection</Link></li>
                 {
                     props.currentUser.username
                         ? <li id="list3" onClick={props.handleLogout} className="nav-list"><LogOut doSetCurrentUser={props.doSetCurrentUser}/></li>
-                        : <li id="list3" className="nav-list"><Link to='/login'>//Log in</Link></li>
+                        : 
+                        (
+                            <div className="ternary-nav">
+                                <li id="list3" className="nav-list"><Link to='/login'>//Log in</Link></li>
+                                <li id="list4" className="nav-list"><Link to='/register'>//Register</Link></li>
+                            </div>
+                        )
+                       
                 }
-                <li id="list4" className="nav-list"><Link to='/register'>//Register</Link></li>
             </ul>
         </div> 
     )
