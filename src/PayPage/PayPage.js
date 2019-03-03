d4f4acaf1d1f47df507d9ecbef64d8cdf4e69da9
Brent Ahrens <cidida@gmail.com>
2019-03-03 12:07:41 -0800

    finishes pay page

--- src/PayPage/PayPage.js
+++ src/PayPage/PayPage.js
@@ -5,7 +5,19 @@ import './PayPage.css'
 const PayPage = () => {
     return(
         <div className="Pay-Container">
+            <form className="pay-form-flex">
             <h1 className="pay-header">PayPal</h1>
+            <label>
+              <input  type='text' name='username' placeholder="Shipping" autoComplete="off"/>
+            </label>
+            <label>
+              <input  type='email' name='email' placeholder="Billing"  autoComplete="off"/>
+            </label>
+            <label>
+              <input  className="input-margin" type='password' name='password' placeholder="Card info" autoComplete="off"/>
+            </label>
+            <button className="checkout-btn" type='submit'>Checkout</button>
+          </form>
         </div>
     )
 }
